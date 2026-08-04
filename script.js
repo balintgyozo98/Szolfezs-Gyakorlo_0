1d84ea3011c83d6eb158a2cb00c2b14dc7f0d8db
balintgyozo98 <93055710+balintgyozo98@users.noreply.github.com>
2026-08-04 07:42:56 +0200

    Add files via upload
    First commit.

---
+++ script.js
@@ -0,0 +1,16 @@
+const feladat = document.getElementById("feladat");
+
+document.getElementById("ujFeladat").onclick = function(){
+
+    feladat.innerHTML = "Tiszta kvint felfelé";
+
+    // ide fog később kerülni
+    // a kezdőhang lejátszása
+};
+
+document.getElementById("megoldas").onclick = function(){
+
+    // ide kerül majd a megoldó hang lejátszása
+
+    alert("Itt fog megszólalni a helyes hang.");
+};
